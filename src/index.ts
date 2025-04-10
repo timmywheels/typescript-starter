f635daeff0e1383f55fec5491921ab41d81d4463
Tim Wheeler <tim@timwheeler.com>
2025-04-10 06:48:06 -0400

    configure for esm modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import * as dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 console.log("API_KEY:", process.env.API_KEY);
+// top-level await
+const response = await fetch("http://google.com");
+console.log(response);
